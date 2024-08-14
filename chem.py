c523db1af3f5d04c9e956f4112455969d5f71bad
aleksandra_khotnyuk <159049250+alexandrahotnuk@users.noreply.github.com>
2024-08-14 20:23:44 +0000

    add chem.py

--- chem.py
+++ chem.py
@@ -1,1 +1,8 @@
+import pandas as pd
+from chembl_webresource_client.new_client import new_client
 
+available_resources = [resource for resource in dir(new_client) if not resource.startswith('_')]
+print(available_resources)
+
+brain_target = new_client.target.search('brain')
+print(brain_target)
